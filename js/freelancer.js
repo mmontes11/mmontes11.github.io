4153b0e8a2eca2597aec9e95fa3692fde73851e4
unknown <mmontes@ITG0120.itgnet.itg.es>
2015-04-15 15:18:03 +0200

    Body project

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,10 +1,3 @@
-/*!
- * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('body').on('click', '.page-scroll a', function(event) {
         var $anchor = $(this);
@@ -15,7 +8,6 @@ $(function() {
     });
 });
 
-// Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
         $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
@@ -26,12 +18,19 @@ $(function() {
     });
 });
 
-// Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
-// Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
+});
+
+$('#tab a').click(function (e) {
+    if ($(this).parent('li').hasClass('active')) {
+        $($(this).attr('href')).hide();
+    } else {
+        e.preventDefault();
+        $(this).tab('show');
+    }
 });
